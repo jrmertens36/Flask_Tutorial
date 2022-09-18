aa3941eaa863e9a091559da884247978745dead8
jrmertens36 <36986210+jrmertens36@users.noreply.github.com>
2022-09-18 10:28:06 +0200

    Anlegen einer neuen comments.txt, wenn nicht vorhanden.

--- comments.py
+++ comments.py
@@ -4,9 +4,16 @@ comments = Blueprint('comments', __name__)
 
 @comments.route('/comments')
 def comment():
-    f = open('comments.txt', 'r')
-    comments = f.readlines()
-    f.close()
+
+    comments=""
+    try:
+        f = open('comments.txt', 'r')
+        f.seek(0)
+        comments = f.readlines()
+        f.close()
+    except:
+        f = open('comments.txt', 'x')
+        f.close()
 
     return render_template('comments.html', comments=comments)
 
