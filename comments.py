e044274f5c2e226871268742607009ebdceea2de
jrmertens36 <36986210+jrmertens36@users.noreply.github.com>
2022-09-22 19:26:30 +0200

    Version mit unsicherer Ausgabe (javascript in Kommentaren wird ausgeführt)

--- comments.py
+++ comments.py
@@ -26,3 +26,43 @@ def comment_post():
     f.close()
 
     return redirect(url_for('comments.comment'))
+
+@comments.route('/insecure')
+def insecure():
+
+    comments=""
+    try:
+        f = open('comments.txt', 'r')
+        f.seek(0)
+        comments = f.readlines()
+        f.close()
+    except:
+        f = open('comments.txt', 'x')
+        f.close()
+
+
+    document = """<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HSS Flask Tutorial</title>
+    <link rel="stylesheet" href="static/css/commments.css" >
+</head>
+
+<body>
+        <header class="header">
+            <img src="static/HSS Logo.png" class="logo" height='50px' >
+        </header>
+
+        <div class="hss-body">
+            <div class="container has-text-centered">"""
+
+    for c in comments:
+        document = document +"<br>" + c
+
+    document = document + " </div>  </div> </body>"
+
+    return document
