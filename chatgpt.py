bc777d2c46eb151d9ec0d07540679d3f30655b16
juergenmertens <36986210+jrmertens36@users.noreply.github.com>
2023-11-13 19:36:01 +0100

    Interface zu openai neu

--- chatgpt.py
+++ chatgpt.py
@@ -1,19 +1,26 @@
-import openai
 from flask import current_app
+from openai import OpenAI
 
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key='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'
+)
 
 def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
 
-    openai.api_key = current_app.config.get('OPENAI_APIKEY')
-    openai.org_key = current_app.config.get('OPENAI_ORGKEY')
 
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
         model=model,
-        messages=messages,
-        temperature=temperature  # this is the degree of randomness of the model's output
+        temperature=temperature,
     )
-    return response.choices[0].message["content"]
+
+    return response.choices[0].message.content
 
 
 def check_comment(comment):
